5760eb7df3285d7fcfe946f59e84aefefe387147
Silas Santos <sillassanttos@gmail.com>
2020-01-27 21:09:26 -0400

    Commit inicial dos arquivos do projeto

---
+++ src/test/java/com/gmail/sillassanttos/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.sillassanttos.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
